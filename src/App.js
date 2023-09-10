2bfb68d6b37d789cbff4c3c3583e7eb7da5a54dc
HasinduUdantha <hasinduudantha21@gmail.com>
2023-09-11 00:28:57 +0530

    add css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './App.css';
+import { useReducer } from "react";
 import "./styles.css"
 
+
+function reducer(state, action){
+
+  
+}
 function App() {
+  const [state, dispatch] = useReducer(reducer, 
+    {})
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">
+        <div className="previous-operand"></div>
           <div className="current-operand"></div>
-        </div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
@@ -15,6 +21,7 @@ function App() {
       <button>1</button>
       <button>2</button>
       <button>3</button>
+      <button>*</button>
       <button>4</button>
       <button>5</button>
       <button>6</button>
@@ -22,6 +29,7 @@ function App() {
       <button>7</button>
       <button>8</button>
       <button>9</button>
+      <button>-</button>
       <button>.</button>
       <button>0</button>
       <button className='span-two'>=</button>
